3122b740c3d393b5a4bf28941187358d8ecaeadc
Rémy HUBSCHER <rhubscher@mozilla.com>
2016-11-24 12:46:50 +0100

    Add a tests.

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -47,3 +47,13 @@ class RoundTrip(unittest.TestCase):
         # Check that identical to original file.
         generated = output.getvalue()
         assert self.original == generated
+
+    def test_full_dump(self):
+        cmd = 'kinto-wizard {} --server={} --auth={} --full'
+        load_cmd = cmd.format("dump", self.server, self.auth)
+        sys.argv = load_cmd.split(" ")
+        output = io.StringIO()
+        with redirect_stdout(output):
+            main()
+        output.flush()
+        assert 'last_modified' in output.getvalue()
